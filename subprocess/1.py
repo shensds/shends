92a9c02ab00b1478f3f0102a5041fd722876a58a
shen <shensds@163.com>
2019-04-01 23:32:44 +0800

    xxx

--- subprocess/1.py
+++ subprocess/1.py
@@ -29,23 +29,44 @@ def get_status_output(cmd):
 
 
 
+import subprocess
+import sys
+
+
+# 常用编码
+GBK = 'gbk'
+UTF8 = 'utf-8'
+
+# 解码方式，一般 py 文件执行为utf-8 ，cmd 命令为 gbk
+current_encoding = GBK
+
+
+popen = subprocess.Popen(['ping', 'www.baidu.com'],
+                         stdout = subprocess.PIPE,
+                         stderr = subprocess.PIPE,
+                         bufsize=1)
+
+# 重定向标准输出
+while popen.poll() is None:         # None表示正在执行中
+    r = popen.stdout.readline().decode(current_encoding)
+    sys.stdout.write(r)                    # 可修改输出方式，比如控制台、文件等
+
+# 重定向错误输出
+if popen.poll() != 0:                      # 不为0表示执行错误
+    err = popen.stderr.read().decode(current_encoding)
+    sys.stdout.write(err)                 # 可修改输出方式，比如控制台、文件等
 
 
 
-# class Shell(object) :
-    # def runCmd(self, cmd) :
-        # res = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        # sout ,serr = res.communicate() 
-        # return res.returncode, sout, serr, res.pid
 
-# shell = Shell()
 
-# returncode, sout, serr, pid = shell.runCmd("ifconfig")
 
-# print(returncode)
-# print(sout.decode())
-# print(serr)
-# print(pid)
+
+
+
+
+
+
 
 
 obj = subprocess.Popen(["python3"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
