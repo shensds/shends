400d49d498dfd22dcb4f57c01979038bbfde3826
shen <shensds@163.com>
2019-04-07 15:46:45 +0800

    xxx

--- dome/telnet/TelnetClient.py
+++ dome/telnet/TelnetClient.py
@@ -2,64 +2,60 @@ import logging
 import telnetlib
 import time
 from  threading import Thread
+
+
 class TelnetClient():
     def __init__(self,host_ip,port):
         self.host_ip = host_ip
         self.port = port
-        self.tn = telnetlib.Telnet(self.host_ip,port=self.port)
-        self.st = 1
         self.prompt = '$'.encode()
+        try:
+            self.tn = telnetlib.Telnet(self.host_ip,port=self.port)
+        except:
+            print(self.host_ip+":"+self.port+"连接失败")
+        text = self.tn.read_until("Welcome to IPOP share server".encode(),timeout=10)
+        print(self.host_ip+":"+self.port+"连接成功")
     def operate(self):
-        #self.thread1 = Thread(target = self.wocao)
-        #self.thread1.start()
-        a = "pwd"
-        #self.tn.write(xxx)
-        #self.tn.write(xxx)
-        self.sendCmd(a)
-        time.sleep(1)
-        self.tn.write(b"\n")
-        time.sleep(1)
-        self.tn.write(b"\n")
-        time.sleep(1)
-        self.tn.write(b"\n")
-        time.sleep(1)
-        self.tn.write(b"\n")
-        time.sleep(1)
-        self.tn.write(b"\n")
-        time.sleep(1)
-        self.tn.write(b"pwd\n")
-        time.sleep(1)
-        self.tn.write(b"\n")
-        time.sleep(1)
-        self.sendCmd(a)
-
-
-        print("-------------")
-
-
-
+        
+        self.sendCmd("echo 1")
+        self.sendCmd("echo 2")
+        self.sendCmd("echo 3")
+        self.sendCmd("echo 4")
+        self.sendCmd("echo 5")
+        
     def sendCmd(self, func, timeOut = 10, expect = None, match = None, printMsg = True,re = True):
         beginTime = time.time()
-        xxx = func.encode('ascii') + b'\n'
-        time.sleep(1)
-        self.tn.write(xxx)
-        print(xxx)
+        self.tn.write(func.encode('ascii') + b'\n')
         if re == True:
-            time.sleep(1)
-            text = self.tn.read_until(b"root",timeout=10)
-            print("xxxxxxxxxxxxx")
+            text = self.tn.read_until(self.prompt,timeout=10)
+            text = text.decode().strip()
+            if printMsg:
+                print ("+++++ 时间: %s 耗时 %.3fs " % (time.strftime('%X', time.localtime(beginTime)), time.time() - beginTime))
+                print ("+++++ 命令: '%s' " % (func))
+                print ("+++++++++++++++++ 命令回显开始 ++++++++++++++++++")
+                print (text)
+                print ("----------------- 命令回显结束-------------------\n")
+            ret = True
+            if expect != None:
+                if -1 == text.find(expect):
+                    ret = False
+            if match != None:
+                rst = re.search(expect, text)
+                if rst == None: ret = False
+            return ret, text
 
-
-
-
+        
     def execute_some_command(self,command):
         command_result = self.tn.read_very_eager().decode('ascii')
 
 
 if __name__ == '__main__':
 
-    host_ip = '127.0.0.1'
+    host_ip = '192.168.99.248'
     port = "1000"
+
     tel = TelnetClient(host_ip,port)
     tel.operate()
+
+
 
